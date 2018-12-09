00334c10080d39ee5e7f098e3181f6b7b01aba3a
WANG_Sinan <11849180@mail.sustc.edu.cn>
2018-12-09 13:16:16 +0800

    reformat code and add plot with label

--- data_preprocessor.py
+++ data_preprocessor.py
@@ -2,8 +2,6 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.ticker as tic
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
 from sklearn.neighbors import KNeighborsClassifier, LocalOutlierFactor
 from sklearn.preprocessing import StandardScaler
 import seaborn as sns
@@ -26,7 +24,7 @@ import seaborn as sns
 def plot_feature(features, filename):
     fig, axes = plt.subplots(nrows=2, ncols=3, figsize=(12, 7))
     names = ['SL', 'Time', 'BP', 'Circulation', 'HR', 'EEG']
-    for i, column in enumerate(features.T):
+    for i, column in enumerate(features.values.T):
         x, y = i / 3, i % 3
         axes[x, y].hist(column)
         axes[x, y].xaxis.set_major_locator(tic.MaxNLocator(4))
@@ -51,6 +49,29 @@ def plot_correlation_heatmap(X, filename):
     fig.tight_layout()
     plt.savefig(filename+'.png')
 
+def plot_feature_labeling(X, y, filename):
+    fig, axes = plt.subplots(nrows=6, ncols=6, figsize=(20, 15))
+    features = ['SL', 'Time', 'BP', 'Circulation', 'HR', 'EEG']
+    labels = ['Standing', 'Walking', 'Sitting', 'Falling', 'Cramps', 'Running']
+    for label in range(6):
+        with_label = X[y == label]
+        for feature, column in enumerate(with_label.values.T):
+            axes[label, feature].hist(column)
+            axes[label, feature].xaxis.set_major_locator(tic.MaxNLocator(4))
+    for ax, col in zip(axes[0], features):
+        ax.set_title('#' + col)
+    for ax, row in zip(axes[:,0], labels):
+        ax.set_ylabel(row, rotation=0, size='large', labelpad=30)
+    fig.subplots_adjust(hspace=0.2, wspace=0.2)
+    fig.tight_layout()
+    plt.savefig(filename+'.png')
+
+
+# plot_feature(X_train, 'trainset_plot')
+# plot_feature(X_test, 'testset_plot')
+# plot_pairwise_matrix(X_train, 'pairwise_plot')
+# plot_correlation_heatmap(X_train, 'correlation_plot')
+# plot_feature_labeling(X_train, y_train, 'label_wise_plot')
 
 
 def outlier_detection(data):
@@ -70,56 +91,18 @@ def drop_coulmn(data):
     # return data.drop(['Circulation', 'Time'], axis=1) # 0.875345
     return data.drop(['Circulation', 'HR'], axis=1) # 0.883365
 
-def evaluate_accuracy(model, X, y, testX, testy):
-    model.fit(X, y)
-    predy = model.predict(testX)
-    return pd.Series(predy), accuracy_score(testy, predy)
 
-def write_data(yhat, filename):
-    with open('result/{}.csv'.format(filename), 'w') as w:
-        w.write('Id,Category')
-        for i, y in enumerate(yhat, start=1):
-            w.write('\n{},{}'.format(i, int(y)))
-    print 'write predicted file'
+def fetch_all_data():
+    traindata = pd.read_csv('train.csv')
+    outlier_train = outlier_detection(traindata)
+    traindata = traindata[~outlier_train]
+    X_train = traindata.iloc[:,:6]
+    y_train = traindata.iloc[:,-1]
 
+    X_test = pd.read_csv('test.csv')
+    y_real = pd.read_csv('true_label.csv')
+    # outlier_test = outlier_detection(X_test)
 
-
-traindata = pd.read_csv('train.csv')
-outlier_train = outlier_detection(traindata)
-traindata = traindata[~outlier_train]
-X_train = traindata.iloc[:,:6]
-y_train = traindata.iloc[:,-1]
-
-X_test = pd.read_csv('test.csv')
-y_real = pd.read_csv('true_label.csv')
-# outlier_test = outlier_detection(X_test)
-
-
-X_train = drop_coulmn(X_train)
-X_test = drop_coulmn(X_test)
-
-
-# plot_feature(X_train, 'trainset_plot')
-# plot_feature(X_test, 'testset_plot')
-# plot_pairwise_matrix(X_train, 'pairwise_plot')
-# plot_correlation_heatmap(X_train, 'correlation_plot')
-
-
-
-
-accs = []
-for trial in range(100):
-    rf = RandomForestClassifier(
-        n_estimators=100,
-        bootstrap=False,
-        n_jobs=-1,
-    )
-    y_pred, acc = evaluate_accuracy(rf, X_train, y_train, X_test, y_real)
-    accs.append(acc)
-    print '{} trial: {}'.format(trial+1, acc)
-
-print 'accuracy', np.mean(accs)
-
-# label = 'rforest' + str(int(acc*2000))
-
-# write_data(y_pred, label)
+    # X_train = drop_coulmn(X_train)
+    # X_test = drop_coulmn(X_test)
+    return X_train, y_train, X_test, y_real
